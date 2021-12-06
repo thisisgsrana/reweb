5bbe08c7c9ff3fa31dfb8508eb01911637c9c911
thisisgsrana <gsrs2002@gmail.com>
2021-12-06 23:09:57 +0530

    checking push

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello I am Gagan. 
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         DOnt learn anything DId i do sometingn
         </a>
       </header>
     </div>
